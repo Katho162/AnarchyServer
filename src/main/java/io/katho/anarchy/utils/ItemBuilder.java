7c81854eb12e2daafe30aa6ed6f689928fb4d3bd
Katho162 <katho162@gmail.com>
2019-07-09 10:06:34 -0300

    Implemented ItemBuilder

--- src/main/java/io/katho/anarchy/utils/ItemBuilder.java
+++ src/main/java/io/katho/anarchy/utils/ItemBuilder.java
@@ -1,4 +1,54 @@
 package io.katho.anarchy.utils;
 
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.Arrays;
+import java.util.function.Consumer;
+
 public class ItemBuilder {
+    /**
+     * @author ssk
+     * Just a simple class to turns ItemStack objects easier
+     */
+
+    private ItemStack item;
+
+    public ItemBuilder(ItemStack item) {
+        this.item = item;
+    }
+
+    public ItemBuilder(Material type){
+        this(new ItemStack(type));
+    }
+
+    public ItemBuilder(Material type , int amount){
+        this(new ItemStack(type , 1));
+    }
+
+    public ItemBuilder changeItem(Consumer<ItemStack> consumer){
+        consumer.accept(item);
+        return this;
+    }
+
+    public ItemBuilder changeItemMeta(Consumer<ItemMeta> consumer){
+        ItemMeta itemMeta = item.getItemMeta();
+        consumer.accept(itemMeta);
+        item.setItemMeta(itemMeta);
+        return this;
+    }
+
+    public ItemBuilder name(String name){
+        return changeItemMeta(it -> it.setDisplayName(name));
+    }
+
+    public ItemBuilder setLore(String... lore){
+        return changeItemMeta(it -> it.setLore(Arrays.asList(lore)));
+    }
+
+    public ItemStack build(){
+        return item;
+    }
+
 }
